c6c309aa726d2ecea94a7a7e38f58efa80c208e0
algol <algol.mail@gmail.com>
2024-06-16 12:36:36 +0700

    add restoring session

--- lua/community.lua
+++ lua/community.lua
@@ -30,5 +30,6 @@ return {
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.lsp.nvim-lsp-file-operations" },
   { import = "astrocommunity.recipes.telescope-nvchad-theme" },
+  { import = "astrocommunity.recipes.auto-session-restore" },
   -- { import = "astrocommunity.recipes.telescope-lsp-mappings" },
 }
