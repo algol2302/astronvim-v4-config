81da8c42e366b4c75ae971ae7aaf0bfc703deb1f
algol <algol.mail@gmail.com>
2024-08-10 00:36:05 +0700

    add xml diagnostic

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -71,6 +71,7 @@ return {
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.isort,
       null_ls.builtins.formatting.black,
+      null_ls.builtins.diagnostics.tidy,
       -- null_ls.builtins.formatting.prettier,
       null_ls.builtins.diagnostics.sqlfluff.with {
         extra_args = { "--dialect", "postgres" }, -- change to your dialect
